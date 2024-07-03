37fd492776adc27fa2e882cc97b2e84e8224ffa8
Ibrahim Musa <ibrahimmusa@imusa-sg.local>
2024-07-03 10:26:40 +0100

    job can be created now and be queried

--- src/main/java/com/musa/spring/entity/Job.java
+++ src/main/java/com/musa/spring/entity/Job.java
@@ -1,8 +1,10 @@
 package com.musa.spring.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 
+@Data
 @Entity
 //@Table(name = "job_table")
 public class Job {
@@ -15,9 +17,6 @@ public class Job {
     private String maxSalary;
     private String location;
 
-    @ManyToOne
-    private Company company;
-
     public Job() {
     }
 
@@ -29,61 +28,4 @@ public class Job {
         this.maxSalary = maxSalary;
         this.location = location;
     }
-
-    public Company getCompany() {
-        return company;
-    }
-
-    public void setCompany(Company company) {
-        this.company = company;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getMinSalary() {
-        return minSalary;
-    }
-
-    public void setMinSalary(String minSalary) {
-        this.minSalary = minSalary;
-    }
-
-    public String getMaxSalary() {
-        return maxSalary;
-    }
-
-    public void setMaxSalary(String maxSalary) {
-        this.maxSalary = maxSalary;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
 }
-
