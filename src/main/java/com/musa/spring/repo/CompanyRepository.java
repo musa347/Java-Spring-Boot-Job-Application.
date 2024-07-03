f7f8d5bf449529e3e3ce296717e7ac823e317fee
Ibrahim Musa <ibrahimmusa@imusa-sg.local>
2024-07-03 10:26:23 +0100

    job can be created now and be queried

--- src/main/java/com/musa/spring/repo/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.musa.spring.repo;
-
-import com.musa.spring.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long>{
-
-}
